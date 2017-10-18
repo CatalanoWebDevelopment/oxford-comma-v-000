1ca502a8f888ebe5d58ddb2d22c83487bf8d5be8
Parker Catalano <catalano.work@gmail.com>
2017-10-18 21:36:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    array[array.length - 1] = "and "
+    array[array.length - 1] = "and #{array.last}"
     array.join(", ")
   end
 end
