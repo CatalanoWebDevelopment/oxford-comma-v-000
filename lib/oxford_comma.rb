8eb5ae4e829841d4bdac8e7f2bb6c7d257e5e3b2
Parker Catalano <catalano.work@gmail.com>
2017-10-18 20:47:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-when array.length == 1
-  array.join
-when array.length == 2
-  array.join("and")
-when array.length == 3
-  array.join(", and")
-else
-  array.join(", and")
-end
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join("and")
+  elsif array.length == 3
+    array.join(", and")
+  else
+    array.join(", and")
+  end
 end
